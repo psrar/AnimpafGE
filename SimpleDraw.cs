f2f869c3226e649c30dd1d37e9e7f286ebdb3f11
p.s.rar <pavel.sima203@gmail.com>
2021-05-19 17:26:45 +0300

    First class to draw

---
+++ SimpleDraw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Animpaf
+{
+	class Graphics
+	{
+		static public void SimpleDraw(SpriteBatch batch, Texture2D sprite)
+		{
+			batch.Begin(samplerState: SamplerState.PointClamp);
+			batch.Draw(sprite, Vector2.Zero, null, Color.White);
+			batch.End();
+		}
+		static public void SimpleDraw(SpriteBatch batch, Texture2D sprite, float scale)
+		{
+			batch.Begin(samplerState: SamplerState.PointClamp);
+			batch.Draw(sprite, Vector2.Zero, null, Color.White, 0, Vector2.Zero, scale, SpriteEffects.None, 0);
+			batch.End();
+		}
+	}
+}
