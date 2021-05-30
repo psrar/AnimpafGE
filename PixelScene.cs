0cdca1c149759820b9ca4c9713fa9af7538f8200
p.s.rar <pavel.sima203@gmail.com>
2021-05-30 21:48:16 +0300

    Начало создания пиксельной сцены

---
+++ PixelScene.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AnimpafGE.ECS;
+using AnimpafGE.ECS.Components;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace AnimpafGE.ECS
+{
+	class PixelScene : Scene
+	{
+		public PixelScene(Game game, string name = null) : base(game)
+		{
+			Name = name ?? "SimpleScene";
+		}
+
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			//Write your initialization logic here
+		}
+
+		public override void LoadContent()
+		{
+			//Write your loading code here
+		}
+
+		public override void Process()
+		{
+			//Write your Process (Update) code here
+		}
+
+		public override void Render()
+		{
+			base.Render();
+
+			//Write your render code here
+		}
+	}
+}
